4335adfc5fdf206ab4f76d904ab85b7989365213
Nikos Mouchtaris <mouchtaris@gmail.com>
2015-06-12 15:26:48 +0000

    Properly use hagrid address in startup.bash

--- skeletons/local_xpra/startup.bash
+++ skeletons/local_xpra/startup.bash
@@ -1,7 +1,9 @@
 #!/bin/bash -x
 
+mydir="$( cd "$(dirname "$0")" && /usr/bin/env pwd )"
+
 true \
 && export PATH="/home/nikos/dockintelli:$PATH" \
 && _ssh_enable ide_intelli \
-&& xpra start ssh/nikos@$(_hagrid_address):$(ide:port)/$(ide:display) --no-pulseaudio --start-child /home/nikos/xterm \
+&& xpra start ssh/nikos@$(<"$mydir"/../../_hagrid_address):$(ide:port)/$(ide:display) --no-pulseaudio --start-child /home/nikos/xterm \
 && true
