60e3cf68d74c90d698571d3d747bd020c4646680
Nikos Mouchtaris <mouchtaris@gmail.com>
2015-06-12 09:35:25 +0000

    Use _hagrid_address for all hagrid situations

--- skeletons/local_xpra/startup.bash
+++ skeletons/local_xpra/startup.bash
@@ -3,5 +3,5 @@
 true \
 && export PATH="/home/nikos/dockintelli:$PATH" \
 && _ssh_enable ide_intelli \
-&& xpra start ssh/nikos@hagrid.magodruidi.com:$(ide:port)/$(ide:display) --no-pulseaudio --start-child /home/nikos/xterm \
+&& xpra start ssh/nikos@$(_hagrid_address):$(ide:port)/$(ide:display) --no-pulseaudio --start-child /home/nikos/xterm \
 && true
