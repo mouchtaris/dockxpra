6dd00c43d746dc38203f9b2e998ec141ff03d409
Nikos Mouchtaris <mouchtaris@gmail.com>
2015-06-22 10:57:13 +0000

    Welcome resolv:

--- skeletons/local_xpra/startup.bash
+++ skeletons/local_xpra/startup.bash
@@ -1,9 +1,10 @@
 #!/bin/bash -x
 
 mydir="$( cd "$(dirname "$0")" && /usr/bin/env pwd )"
+export PATH="$mydir":"$PATH"
 
 true \
 && export PATH="/home/nikos/dockintelli:$PATH" \
 && _ssh_enable ide_intelli \
-&& xpra start ssh/nikos@$(<"$mydir"/../../_hagrid_address):$(ide:port)/$(ide:display) --no-pulseaudio --start-child /home/nikos/xterm \
+&& xpra start ssh/nikos@$(resolv:hagrid):$(ide:port)/$(ide:display) --no-pulseaudio --start-child /home/nikos/xterm \
 && true
